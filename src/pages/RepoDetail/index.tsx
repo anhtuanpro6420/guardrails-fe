92d50aaa36d10906b77cccf2f74c010fbf14c1f2
si <anhtuanpro6420@gmail.com>
2022-01-12 22:30:00 +0700

    show cards

--- src/pages/RepoDetail/index.tsx
+++ src/pages/RepoDetail/index.tsx
@@ -1,10 +1,13 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { List } from 'antd';
+import { Card, List, Typography } from 'antd';
 import { getRepoDetail } from 'apis/repo.api';
 import { IList } from 'interfaces/list.interface';
 import { IRepo } from 'interfaces/repo.interface';
 import './RepoDetail.scss';
+import { ICard } from 'interfaces/card.interface';
+
+const { Text } = Typography;
 
 const RepoDetail: FC = () => {
     const { repoId }: { repoId: string } = useParams();
@@ -19,15 +22,28 @@ const RepoDetail: FC = () => {
         fetchRepoDetail(repoId);
     }, [repoId]);
 
+    const renderListItem = (cards: Array<ICard> = []) => {
+        return cards.map((card: ICard) => {
+            const { id, text } = card;
+            return <Card key={id} title={text} />;
+        });
+    };
+
     const renderList = () => {
         const { lists = [] }: { lists: Array<IList> } = repo || ({} as IRepo);
         return (
             <List
                 grid={{ gutter: 16, column: 4 }}
                 dataSource={lists}
-                renderItem={(list: IList) => (
-                    <List.Item>{list.title}</List.Item>
-                )}
+                renderItem={(list: IList) => {
+                    const { cards = [] } = list || {};
+                    return (
+                        <List.Item>
+                            <Text strong>{list.title}</Text>
+                            {renderListItem(cards)}
+                        </List.Item>
+                    );
+                }}
             />
         );
     };
