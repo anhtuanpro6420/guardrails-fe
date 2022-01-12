23071ec3f1becbfa5b30af21dd18156ebc3b1412
si <anhtuanpro6420@gmail.com>
2022-01-12 21:26:52 +0700

    show repos

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,7 +1,10 @@
+import React, { FC, useEffect, useState } from 'react';
+import { Typography, Space, List } from 'antd';
 import { getRepos } from 'apis/repo.api';
 import { IRepo } from 'interfaces/repo.interface';
-import React, { FC, useEffect, useState } from 'react';
 import './Repo.scss';
+
+const { Text } = Typography;
 
 const Repo: FC = () => {
     const [repos, setRepos] = useState([] as Array<IRepo>);
@@ -15,7 +18,22 @@ const Repo: FC = () => {
         fetchRepos();
     }, []);
 
-    return <div className='repo-page'>Repo page</div>;
+    const renderRepos = () => {
+        return (
+            <List
+                header={<div>Repositories</div>}
+                bordered
+                dataSource={repos}
+                renderItem={(repo: IRepo) => (
+                    <List.Item className='repo-item'>
+                        <Text strong>{repo.name}</Text>
+                    </List.Item>
+                )}
+            />
+        );
+    };
+
+    return <div className='repo-page'>{renderRepos()}</div>;
 };
 
 export default Repo;
