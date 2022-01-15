6a664e759bc9d06f4157a561d068dca4beb9943f
si <anhtuanpro6420@gmail.com>
2022-01-15 21:42:27 +0700

    split repos header

---
+++ src/components/ReposHeader/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import './ReposHeader.scss';
+
+interface Props {
+    onCreateClick: () => void;
+}
+
+const ReposHeader: FC<Props> = ({ onCreateClick }) => {
+    return (
+        <div className='repo-header'>
+            <h1>Repositories</h1>
+            <Button
+                type='primary'
+                icon={<PlusOutlined onClick={onCreateClick} />}
+            />
+        </div>
+    );
+};
+
+export default ReposHeader;
